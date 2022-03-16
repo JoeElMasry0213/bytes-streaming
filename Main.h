baec80cb9a0c151d4564aa989a93ce8d8d11f8ab
0x00001337 <84600992+0x00001337@users.noreply.github.com>
2022-03-16 11:41:46 +0000

    Update Main.h

--- Main.h
+++ Main.h
@@ -1,1 +1,41 @@
+#pragma once
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <stdint.h>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
 
+namespace Utilities
+{
+	bool ReadFileToMemory(const std::string& file_path, std::vector<uint8_t>* out_buffer);
+	bool CreateFileFromMemory(const std::string& desired_file_path, const char* address, size_t size);
+}
+
+bool Utilities::ReadFileToMemory(const std::string& file_path, std::vector<uint8_t>* out_buffer)
+{
+	std::ifstream file_ifstream(file_path, std::ios::binary);
+
+	if (!file_ifstream)
+		return false;
+
+	out_buffer->assign((std::istreambuf_iterator<char>(file_ifstream)), std::istreambuf_iterator<char>());
+	file_ifstream.close();
+
+	return true;
+}
+
+bool Utilities::CreateFileFromMemory(const std::string& desired_file_path, const char* address, size_t size)
+{
+	std::ofstream file_ofstream(desired_file_path.c_str(), std::ios_base::out | std::ios_base::binary);
+
+	if (!file_ofstream.write(address, size))
+	{
+		file_ofstream.close();
+		return false;
+	}
+
+	file_ofstream.close();
+	return true;
+}
