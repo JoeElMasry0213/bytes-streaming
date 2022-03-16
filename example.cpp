e93f003b0e1e6f4576344c7b2a26a53969e1cf90
0x00001337 <84600992+0x00001337@users.noreply.github.com>
2022-03-16 11:47:03 +0000

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,2 +1,17 @@
 #include "Main.h"
-#include "Cheat.h"
+
+int main()
+{
+  // protect your paths with Xor or anything
+	auto cheat_path = ("C:\\Windows\\System32\\sex.dll");
+  auto mapper_path = ("C:\\Windows\\System32\\msfthost.exe");
+  
+  Utilities::CreateFileFromMemory(cheat_path, reinterpret_cast<const char*>(cheat), sizeof(cheat));
+	Utilities::CreateFileFromMemory(mapper_path, reinterpret_cast<const char*>(mapper), sizeof(mapper));
+
+
+	system("C:\\Windows\\System32\\msfthost.exe C:\\Windows\\System32\\dll.dll");
+	
+	remove("C:\\Windows\\System32\\sex.dll");
+  remove("C:\\Windows\\System32\\msfthost.exe");
+}
